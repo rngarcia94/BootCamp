70a1c1784f9bbe5b24c1dce46d3dbddba62d3209
Rodrigo Garcia <rodrigo.ngarcia@mercadolibre.com>
2021-03-29 16:59:51 -0300

    5ta Practica Spring Diploma

---
+++ PractivaJava5/src/main/java/com/obtenerdiploma/obterner_diploma/ObternerDiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.obtenerdiploma.obterner_diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObternerDiplomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObternerDiplomaApplication.class, args);
+    }
+
+}
---
+++ PractivaJava5/src/test/java/com/obtenerdiploma/obterner_diploma/ObternerDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obtenerdiploma.obterner_diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObternerDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
